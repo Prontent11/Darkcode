a27dc570dd773a29f17782ded0a5ad05255c8943
= <yashyadao2001@gmail.com>
2023-12-31 20:04:40 +0530

    versioning untracked files

--- src/app/(pages)/profile/page.tsx
+++
@@ -1,8 +0,0 @@
-import ProblemTable from "@/components/Table";
-import React from "react";
-
-const Profile = () => {
-  return <div>{/* <ProblemTable problem={problems}/> */}</div>;
-};
-
-export default Profile;
--- src/helper/codeToJson.ts
+++
@@ -1,8 +0,0 @@
-export default function codeToJson(code: any) {
-  const formattedCode = code
-    .replace(/\\/g, "\\\\") // Escape backslashes
-    .replace(/"/g, '\\"') // Escape double quotes
-    .replace(/\n/g, "\\n"); // Escape newlines
-
-  return formattedCode;
-}
